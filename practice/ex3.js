85a11d5455f24edfa89999b9461403fe10fbc5be
BISINIGISHIVANI <90177603+BISINIGISHIVANI@users.noreply.github.com>
2021-11-09 21:30:46 +0530

    intial commit

---
+++ practice/ex3.js
@@ -0,0 +1,36 @@
+function reverseString(str){
+    var listOfChars = str.split('');
+    var reversedListOfChar = listOfChars.reverse();
+    var reversedString = reversedListOfChar.join('');
+    return reversedString;
+  }
+  
+  function isStringPalindrome(str){
+    var reversedString = reverseString(str);
+    return str === reversedString;
+  }
+  
+  function getDateAsString(date){
+    var dateInStr = {day: '', month: '', year: ''};
+  
+    if(date.day < 10){
+      dateInStr.day = '0' + date.day;
+     }
+    else {
+      dateInStr.day = date.day.toString();
+    }
+    
+    if(date.month < 10){
+      dateInStr.month = '0' + date.month;
+    }
+    else {
+      dateInStr.month = date.month.toString();
+    }
+    
+    dateInStr.year = date.year.toString();
+    return dateInStr;
+  }
+  
+  var date = {day: 4, month: 2, year: 2020};
+  
+  console.log(getDateAsString(date));
